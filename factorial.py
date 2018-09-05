66bb45066cdc019eee153e6695f81e8780114605
Anupam suman <anupamsumanphed@gmail.com>
2018-09-06 02:01:49 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,14 @@
+def FirstFactorial(num): 
+
+    # code goes here
+    fact = 1
+    if num != 0:
+        for r in range(1,num+1):
+            
+            fact = fact*r
+    if num ==0:
+        fact = 1
+    return fact
+    
+# keep this function call here  
+print FirstFactorial(raw_input())
