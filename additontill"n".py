9989d089d519ea5fdc91559ebe8a10e357a5c67a
Anupam suman <anupamsumanphed@gmail.com>
2018-09-06 16:50:43 +0530

    Create additontill"n".py

---
+++ additontill"n".py
@@ -0,0 +1,10 @@
+def SimpleAdding(num): 
+
+    # code goes here 
+    nu = 0
+    for n in range(1,num+1):
+        nu = nu+n
+    return nu
+    
+# keep this function call here  
+print SimpleAdding(raw_input())
